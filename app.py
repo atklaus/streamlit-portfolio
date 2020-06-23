bd6fe1dfcf4d8fa9a5f5055e446a1e2a330a7890
atklaus <49078569+atklaus@users.noreply.github.com>
2020-06-23 11:51:40 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/antho/Documents/blog/blog.db'
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
