2595304b6c3182dec40ad2f70fa3d6149d54b7b8
atklaus <49078569+atklaus@users.noreply.github.com>
2020-06-24 23:15:04 -0500

    try pull request

--- flask_app.py
+++ flask_app.py
@@ -22,19 +22,11 @@ def about():
 def test():
     return render_template('test.html')
 
-
-@app.route('/update_server', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('./PersonalBlog')
-        origin = repo.remotes.origin
-        repo.create_head('master', 
-    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-        origin.pull()
-        return '', 200
-    else:
-        return '', 400
-
+@app.route('/update_server',methods=['POST'])
+def foo():
+   data = json.loads(request.data)
+   print "New commit by: {}".format(data['commits'][0]['author']['name'])
+   return "OK"
 
 
 @app.route('/version')
