3d5f3ef7df0174c8b5ac0943d045666fa4a90a34
atklaus <49078569+atklaus@users.noreply.github.com>
2020-06-24 22:55:27 -0500

    update_server

--- flask_app.py
+++ flask_app.py
@@ -23,6 +23,20 @@ def test():
     return render_template('test.html')
 
 
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./PersonalBlog')
+        origin = repo.remotes.origin
+        repo.create_head('master', 
+    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        origin.pull()
+        return '', 200
+    else:
+        return '', 400
+
+
+
 @app.route('/version')
 def version():
     return '1.1.1'
