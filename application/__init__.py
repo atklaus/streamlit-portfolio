004dea479758d827a783301f08efe0768859edfe
atklaus <49078569+atklaus@users.noreply.github.com>
2020-06-26 08:18:13 -0500

    wsgi file

--- application/__init__.py
+++ application/__init__.py
@@ -8,25 +8,6 @@ from flask_assets import Environment
 db = SQLAlchemy()
 r = FlaskRedis()
 
-# def create_app():
-#     """Initialize the core application."""
-#     app = Flask(__name__, instance_relative_config=False)
-#     # app.config.from_object('config.Config')
-
-#     # Initialize Plugins
-#     db.init_app(app)
-#     r.init_app(app)
-
-#     with app.app_context():
-#         # Include our Routes
-#         from . import routes
-
-#         # Register Blueprints
-#         # app.register_blueprint(auth.auth_bp)
-#         # app.register_blueprint(admin.admin_bp)
-
-#         return app
-
 def create_app():
     """Construct core Flask application with embedded Dash app."""
     app = Flask(__name__, instance_relative_config=False)
@@ -47,23 +28,3 @@ def create_app():
         compile_static_assets(assets)
 
         return app
-
-# def create_app():
-#     """Construct core Flask application with embedded Dash app."""
-#     app = Flask(__name__, instance_relative_config=False)
-#     # app.config.from_object('config.Config')
-
-#     with app.app_context():
-#         # Import Flask routes
-#         from application import routes
-
-#         # Import Dash application
-#         from application.plotlydash.dashboard import create_dashboard
-#         app = create_dashboard(app)
-
-#         # Compile CSS
-#         from application.assets import compile_assets
-#         compile_assets(app)
-
-#         return app
-
