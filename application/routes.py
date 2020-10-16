5c62f0ce6e19d2c5325b0086813eb9ade6787ce5
atklaus <klausa@humancapital.com>
2020-10-16 15:07:53 -0500

    spacing error

--- application/routes.py
+++ application/routes.py
@@ -39,14 +39,14 @@ def testdash():
     )
 
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('path/to/git_repo')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 
