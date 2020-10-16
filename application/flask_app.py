e788132d127660fb1532e6c042029948e02f15d1
atklaus <klausa@humancapital.com>
2020-10-16 15:23:29 -0500

    Update flask_app.py

--- application/flask_app.py
+++ application/flask_app.py
@@ -32,6 +32,17 @@ def version():
     return '1.1.1'
 
 
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/atklaus/PersonalBlog.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
