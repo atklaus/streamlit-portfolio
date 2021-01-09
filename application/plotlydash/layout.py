9ebbf5587472665694c193f86c0836db71b6efe1
Adam Klaus <49078569+atklaus@users.noreply.github.com>
2021-01-09 14:39:19 -0600

    remove string replacement

--- application/plotlydash/layout.py
+++ application/plotlydash/layout.py
@@ -38,16 +38,4 @@ DASH_FOOTER = '''
 '''
 
 
-about = '''{{url_for(\'index\')}}'''
-index = '''{{ url_for(\'about\') }}'''
-about_new = r'''https://almostdatascience.com'''
-index_new = r'''https://almostdatascience.com/about'''
-# main = '''"almostdatascience.com"'''
-
-NAV_HTML = NAV_HTML.replace(about,about_new)
-NAV_HTML = NAV_HTML.replace(index,index_new)
-
-# NAV_HTML = NAV_HTML.replace("{{url_for('about')}}",'''"https://almostdatascience.com/about"''')
-
-
 HTML_LAYOUT = DASH_HEADER + NAV_HTML + CUSTOM_STRING + DASH_FOOTER + FOOTER_HTML
