346aa642602cc86d7a776c441bc942012d803c9d
Adam Klaus <49078569+atklaus@users.noreply.github.com>
2021-01-09 14:25:16 -0600

    change app icon reference

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_redis import FlaskRedis
 from flask_assets import Environment
 
-
 # Globally accessible libraries
 db = SQLAlchemy()
 r = FlaskRedis()
